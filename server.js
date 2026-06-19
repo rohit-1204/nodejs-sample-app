ac1f712a381ececcc48704bc78e899cba5578dad
rohit-1204 <73640744+rohit-1204@users.noreply.github.com>
2026-06-20 00:18:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Rohit! Node.js app is running.');
 });
-
+app.get('/health', (req, res) => {
+  res.send('Hello Rohit! Node.js app helth is ok');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
